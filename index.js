72ff95040e1d73ff145e6e0684e9b655544754a3
Abdur Rahman <rahmanx661@gmail.com>
2023-06-23 12:16:06 +0600

    delete method add

--- index.js
+++ index.js
@@ -49,6 +49,13 @@ app.post('/user', async (req, res) => {
     res.send(result);
 })
 
+app.delete('/user/:id',async(req,res)=>{
+    const id=req.params.id;
+    const query={_id: new ObjectId(id)}
+    const result=await userCollection.deleteOne(query);
+    res.send(result)
+  })
+
 
 
 async function run() {
